6a1158eaee22743474ac739a2d7d671bc30db60f
Matthew Tai <matthewtai@gmail.com>
2022-08-10 02:46:25 -0400

    Clio Quiz Incoming

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Clio Quiz Incoming
         </p>
         <a
           className="App-link"
